1c42c760e441863d8bb942cb886289205d90fc85
amaksix <31368619+amaksix@users.noreply.github.com>
2025-09-06 12:57:32 +0300

    initial commit

---
+++ components/CreativeAgency/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="about section-padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11">
+          <span class="sub-title bord mb-30">Who We Are?</span>
+          <h3 class="text-u text-indent ls1">We connect design and technology to help businesses build a clear identity and grow with confidence.</h3>
+        </div>
+        <div class="col-lg-7 offset-lg-5">
+          <div class="text mt-50">
+            <p>At IDigex, we believe that a modern brand should be both stylish and functional.
+            We develop websites, create branding, logos, and 3D solutions that help companies stand out, speak the same language as their audience, and leave a strong impression.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
