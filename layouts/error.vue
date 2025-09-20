842b726e1c21c225d080cf56801215ba59b75736
amaksix <31368619+amaksix@users.noreply.github.com>
2025-09-20 23:10:26 +0300

    404 page

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="error-page">
+    <h1 v-if="error.statusCode === 404">404 - Page Not Found</h1>
+    <h1 v-else>{{ error.statusCode }} - Something Went Wrong</h1>
+    <p>{{ error.message }}</p>
+    <nuxt-link to="/">Go back</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => ({ statusCode: 500, message: 'Oops!' }),
+    },
+  },
+}
+</script>
+
+<style scoped>
+.error-page {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
---
+++ pages/[...all].vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="not-found">
+    <div class="not-found-content-container">
+        <h1>404</h1>
+        <h2>Ups, lapa nav atrasta</h2>
+         <NuxtLink to="/" class="more mt-45 animsition-link not-found-button">
+                <span>Sākums</span>
+              </NuxtLink>
+    </div>
+  </div>
+</template>
